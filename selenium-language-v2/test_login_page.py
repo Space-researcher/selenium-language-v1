b52f87ccf9428125917f4b0f2c9196ffad7765af
Sergii V <46484494+Space-researcher@users.noreply.github.com>
2025-07-21 16:08:09 +0300

    Update test_login_page.py

--- selenium-language-v2/test_login_page.py
+++ selenium-language-v2/test_login_page.py
@@ -1,6 +1,6 @@
 '''
+Lesson link
 https://stepik.org/lesson/238819/step/8?auth=login&unit=211271
-Файл с тестами
 '''
 from pages.login_page import LoginPage
 
@@ -13,9 +13,9 @@ def test_can_open_login_page(browser):
 
 def test_guest_login(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.should_be_login_form()          # выполняем метод страницы — переходим на страницу логина
+    page = LoginPage(browser, link)   # initialize Page Object, pass the driver instance and URL address to the constructor
+    page.open()                      
+    page.should_be_login_form()       
 
 def test_guest_can_register(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
