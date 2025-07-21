758b9db0fb6177413fa1ca3bb17927bef0c31436
Sergii V <46484494+Space-researcher@users.noreply.github.com>
2025-07-21 16:16:26 +0300

    Update test_product_page.py

--- selenium-language-v2/test_product_page.py
+++ selenium-language-v2/test_product_page.py
@@ -1,5 +1,5 @@
 '''
-Запуск
+Run
 pytest -v --tb=line --language=en -m need_review .\test_product_page.py
 '''
 from pages.login_page import LoginPage
@@ -7,41 +7,41 @@ from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 import pytest
 import time
-import os, random, string   # для генерации пароля
+import os, random, string   # to generate a password
 
 
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        # Переход сразу на страницу регистрации;
+        # Go directly to the registration page;
         self.link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
         self.page = LoginPage(browser, self.link)
         self.page.open()
-        # Создание фейковых креденшалов
+        # Creating fake credentials
         template = str(time.time()) + "@fakemail.org"
         email = template[11:]
         length = 10
         chars = string.ascii_letters + string.digits + '!@#$%^&*()'
         random.seed = (os.urandom(1024))
         password = ''.join(random.choice(chars) for i in range(length))
-        # Вывод в консоль для удобства
+        # Output to console for convenience
         print("Credentials: ", email, password)
-        # Зарегистрировать нового пользователя;
+        # Register a new user;
         self.page.register_new_user(email, password)
-        # Проверка, что пользователь залогинен (метод из bas_page.py)
+        # Checking if the user is logged in (method from bas_page.py)
         self.page.should_be_authorized_user()
 
     @pytest.mark.need_review
     def test_user_cant_see_success_message(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
-        # Передаём в первый тест страницу: http://selenium1py.pythonanywhere.com/en-gb/accounts/login/
-        # После регистрации автоматически попадаем на https://selenium1py.pythonanywhere.com/en-gb/
-        # Заказ не делаем - соответственно, выплывающего элемента нет
-        page.should_not_be_success_message()  # Проверяем, есть ли сообщение об успешном добавлении товара (метод из product_page.py)
+        # Passing the page to the first test: http://selenium1py.pythonanywhere.com/en-gb/accounts/login/
+        # After registration, we are automatically redirected to https://selenium1py.pythonanywhere.com/en-gb/
+        # We do not make an order - accordingly, there is no pop-up element
+        page.should_not_be_success_message()  # Check if there is a message about successful addition of the product (method from product_page.py)
 
-    # Параметризация отключена
-    # тест 7 должен падать из-за названия книги "Coders at Work book" вместо "Coders at Work"
+    # Parameterization disabled
+    # test 7 should fail because the book title is "Coders at Work book" instead of "Coders at Work"
     # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     #                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
     #                                   marks=pytest.mark.xfail),
@@ -50,7 +50,7 @@ class TestUserAddToBasketFromProductPage():
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
-        page.should_be_able_by_a_product() # Вызываются методы заказа make_an_order() и решения задачи solve_quiz_and_get_code()
+        page.should_be_able_by_a_product() # The order methods make_an_order() and solve_quiz_and_get_code() are called.
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
@@ -71,7 +71,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    # Переход на страницу логина
-    # page.go_to_login_page_invalid_link() #  В этом случае будет фейл из-за неправильного селектора
+    # Transition to the login page
+    # page.go_to_login_page_invalid_link() # In this case there will be a fail due to the wrong selector
     page.go_to_login_page()
     page.should_be_login_link()
