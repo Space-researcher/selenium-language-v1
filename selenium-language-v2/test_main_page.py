5b42ad6c3bd360b33931a5f15cd017f17c83569a
Sergii V <46484494+Space-researcher@users.noreply.github.com>
2025-07-21 16:12:21 +0300

    Update test_main_page.py

--- selenium-language-v2/test_main_page.py
+++ selenium-language-v2/test_main_page.py
@@ -1,7 +1,7 @@
 '''
-Запуск конкретного теста
+Run single test
  pytest -s .\test_main_page.py::test_guest_cant_see_product_in_basket_negative
-Запуск всех тестов
+Run all tests
  pytest -v --tb=line --language=en .\test_main_page.py
 '''
 from pages.main_page import MainPage
@@ -9,18 +9,18 @@ from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 import pytest
 
-@pytest.mark.login_guest    # запуск pytest -s -m login_guest .\test_main_page.py
+@pytest.mark.login_guest    # run  pytest -s -m login_guest .\test_main_page.py
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        # Initialize the Page Object, pass the driver instance and URL address to the constructor
         page = MainPage(browser, link)
-        page.open()                      # открываем страницу
-        # Медод 1 переход на новую страницу
+        page.open()                      
+        # Method 1 transition to new page
         #  login_page = page.go_to_login_page()
-        # у вызываемого метода был  return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # the called method had  return LoginPage(browser=self.browser, url=self.browser.current_url)
         #  login_page.should_be_login_page()
-        # Медод 2 переход на новую страницу - Инициализируем LoginPage в теле теста
+        # Method 2 transition to a new page - Initialize LoginPage in the test body
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
@@ -42,7 +42,7 @@ def test_guest_cant_see_product_in_basket_negative(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = BasketPage(browser, link)
     page.open()
-    page.add_to_basket() # Нажать "Купить"
-    page.should_be_basket_empty() # Ожидаемо тест упадёт
+    page.add_to_basket() # Press "Buy"
+    page.should_be_basket_empty() # Expected Fail
 
 
