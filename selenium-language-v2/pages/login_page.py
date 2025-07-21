69caeb2022eb958f45cb843be554ce0471cd7b50
Sergii V <46484494+Space-researcher@users.noreply.github.com>
2025-07-21 16:25:51 +0300

    Update login_page.py

--- selenium-language-v2/pages/login_page.py
+++ selenium-language-v2/pages/login_page.py
@@ -1,35 +1,33 @@
 '''
-файл с проверками
+File with checks
 '''
 
 from .locators import LoginPageLocators
 from .base_page import BasePage
 
-# Наследование от BasePage
+# Inheriting from BasePage
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
-        #self.register_new_user()  # Работает и без этого, не ругается на недостающие параметры
+        #self.register_new_user()  # It works without this and doesn't complain about missing parameters.
 
     def should_be_login_url(self):
-        # https://stepik.org/lesson/36285/step/9?unit=162401
-        # новый линк для раздела  4.2, 8 out of 12 steps
-        assert "login" in self.browser.current_url, "Открылась не та страница, login отсутствует"
+        assert "login" in self.browser.current_url, "The wrong page opened, login is missing"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        # implement a check that there is a login form
         # assert True
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        # implement a check that there is a registration form on the page
         # assert True
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
     def register_new_user(self, email, password):
-        # принимает две строки и регистрирует пользователя. Реализуйте его, описав соответствующие элементы страницы.
+        # takes two strings and logs the user in. Implement it by describing the appropriate page elements.
 
         r1 = self.browser.find_element(*LoginPageLocators.REGISTERATION_EMAIL)   # find_element но НЕ find_elementS
         r1.send_keys(email)
