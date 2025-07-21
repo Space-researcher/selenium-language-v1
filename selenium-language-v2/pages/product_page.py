afaf1133f9a6ab77cccf9066eed35158f803d22a
Sergii V <46484494+Space-researcher@users.noreply.github.com>
2025-07-21 16:30:31 +0300

    Update product_page.py

--- selenium-language-v2/pages/product_page.py
+++ selenium-language-v2/pages/product_page.py
@@ -1,27 +1,27 @@
 
 '''
-Класс Page Object страницы товара для теста test_product_page.py
+Class Page Object of the product page for the test test_product_page.py
 '''
 from selenium.common.exceptions import NoSuchElementException
 from .locators import ProductPageLocators
 from .base_page import BasePage
 import time
 
-# Наследование от BasePage
+# Inheriting from BasePage
 class ProductPage(BasePage):
     def should_be_product_page(self):
         self.should_be_able_by_a_product()
 
     def should_be_able_by_a_product(self):
-        # Разное поведение, в зависимости от того, откуда вызывается эта проверка, зарегистрирован пользователь или нет
+        # Different behavior depending on where this check is called from, whether the user is logged in or not
         try:
-            # Если сразу находимся на странице описания книги
+            # If we are immediately on the book description page
             name_element = self.browser.find_element(*ProductPageLocators.NAME)
             name1 = name_element.text
             price_element = self.browser.find_element(*ProductPageLocators.PRICE)
             price1 = price_element.text
         except NoSuchElementException:
-            # Если попали на страницу приветствия после регистрации пользователя
+            # If you got to the welcome page after user registration
             button0 = self.browser.find_element(*ProductPageLocators.CHOSE_PROPOSED_BOOK)
             button0.click()
             time.sleep(1)
@@ -31,10 +31,10 @@ class ProductPage(BasePage):
             price1 = price_element.text
 
         time.sleep(5)
-        self.make_an_order()    # Делаем заказ (метод из base_page.py)
-        # self.solve_quiz_and_get_code() # Решаем задачу - тут это отключено
+        self.make_an_order()    # Making an order (method from base_page.py)
+        # self.solve_quiz_and_get_code() # Solve the problem - it is disabled here
 
-        # Ищем сообщение о добавлении книги в корзину
+        # Looking for a message about adding a book to the cart
         # HTML code
         # <div class="alertinner "> <strong>Coders at Work</strong> has been added to your basket. </div>
         div1 = self.browser.find_element(*ProductPageLocators.DIV_1)
@@ -42,21 +42,21 @@ class ProductPage(BasePage):
         rest = div1.text.replace(book, '').strip()
 
         print(f' Заказ: {book} {rest}')
-        # Просмотр корзины
+        # View Cart
         time.sleep(2)
         button2 = self.browser.find_element(*ProductPageLocators.VIEW_BASKET)
         button2.click()
         time.sleep(2)
-        # Ждем загрузки корзины
-        # Поиск цены и названия товара в отдельных методах
+        # Waiting for the basket to load
+        # Search for price and product name in separate methods
         price2 = self.find_price_in_backet()
         name2 = self.find_name_in_backet()
         time.sleep(2)
 
-        # Проверка отображения во сплывающем сообщении
+        # Checking the display in a pop-up message
         assert book == name1, "Name is not expected during adding"
 
-        # Проверка отображения в корзине.
+        # Checking the display in the basket.
         assert name1 == name2, "Name is not expected"
         assert price1 == price2, "Price is not expected"
 
