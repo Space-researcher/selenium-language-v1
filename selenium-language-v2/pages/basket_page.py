5f189cd531807eaa001be2cf2f8a8960cad2ec29
Sergii V <46484494+Space-researcher@users.noreply.github.com>
2025-07-21 16:23:16 +0300

    Update basket_page.py

--- selenium-language-v2/pages/basket_page.py
+++ selenium-language-v2/pages/basket_page.py
@@ -23,17 +23,6 @@ class BasketPage(BasePage):
         link.click()
         time.sleep(1)
         try:
-            # Тест ждёт появления элемента 10 сек.
-            # 1) Можно ожидать так, но это не ускоряет процесс ожидания
-            # p_element = WebDriverWait(self.browser, 1).until(lambda d: d.find_element(*BasketPageLocators.BASKET_IS_EMPTY))
-
-            # 2) Можно использовать готовый метод ожидания из base_page.py но в итоге тоже 10 сек ожидания :(
-            # возможно, это из-за try-except блока?
-            # assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY), "No message in the basket"
-
-            # Как создать ЕС метод ожидания прямо тут и передавать (how, what) - не стал разбираться
-            # WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((how, what)))
-
             p_element = self.browser.find_element(*BasketPageLocators.BASKET_IS_EMPTY)
             full_text = p_element.text
             # full_text will be: "Your basket is empty. Continue shopping"
@@ -41,7 +30,7 @@ class BasketPage(BasePage):
             print("Message in the basket: ", message)
             flag = False
             for word in self.languages:
-                if message == self.languages[word]:  # сверяемся со значением в словаре языков
+                if message == self.languages[word]:  # check the meaning in the language dictionary
                     print("Anything is ok")
                     flag = True
                     break
